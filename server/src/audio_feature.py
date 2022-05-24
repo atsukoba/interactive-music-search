0552b42b036b35b951fb2fef4755427e50a99a11
atsukoba <atsuya@sfc.keio.ac.jp>
2022-05-24 19:31:03 +0900

    changed audio feature calc

--- server/src/audio_feature.py
+++ server/src/audio_feature.py
@@ -6,11 +6,10 @@ from typing import List
 
 import librosa
 from sklearn.preprocessing import minmax_scale
-from src.utils import create_logger, env
+from utils import AudioPath, create_logger, env
 
 logger = create_logger(os.path.basename(__file__))
 
-AudioPath = str
 audio_files: List[AudioPath] = glob(os.path.join(
     env["DATASET_PATH"], "spotify_sample", "**", "*.mp3"), recursive=True)
 
@@ -22,16 +21,15 @@ def normalize(x, axis=0):
 def extract_features(path: AudioPath):
     y, sr = librosa.load(path)
     zero_crossings = librosa.zero_crossings(y, pad=False)
-    y_harm, y_perc = librosa.effects.hpss(y)
-    spectral_centroids = librosa.feature.spectral_centroid(y=y, sr=sr)[0]
-    spectral_rolloff = librosa.feature.spectral_rolloff(y=y, sr=sr)[0]
-    mfccs = librosa.feature.mfcc(y=y, sr=sr)
-    mfccs = normalize(mfccs, axis=1)
+    harmonic, percussive = librosa.effects.hpss(y)
+    spectral_centroids = librosa.feature.spectral_centroid(y=y, sr=sr)
+    spectral_rolloff = librosa.feature.spectral_rolloff(y=y, sr=sr)
+    mfccs = normalize(librosa.feature.mfcc(y=y, sr=sr), axis=1)
     chromagram = librosa.feature.chroma_stft(y=y, sr=sr, hop_length=512)
     return [
         zero_crossings,
-        y_harm,
-        y_perc,
+        harmonic,
+        percussive,
         spectral_centroids,
         spectral_rolloff,
         mfccs,
