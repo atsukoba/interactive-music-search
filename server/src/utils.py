01482bb2b6d8a382e966b7abd45d26890493d340
atsukoba <atsuya@sfc.keio.ac.jp>
2022-06-16 11:09:30 +0900

    added some functions

--- server/src/utils.py
+++ server/src/utils.py
@@ -2,11 +2,12 @@ import hashlib
 import json
 import logging
 import os
-from datetime import datetime
 import pickle
-from typing import Literal
-from miditoolkit.midi.parser import MidiFile
+from datetime import datetime
+from typing import Dict, Literal, Optional, TypedDict
+
 import requests
+from miditoolkit.midi.parser import MidiFile
 
 # types
 
@@ -14,24 +15,52 @@ AudioPath = str
 
 # Audio Features
 AudioFeatureName = Literal[
-    "zero_crossings",
+    "zero_crossing_rate",
     "harmonic",
     "percussive",
     "spectral_centroids",
     "spectral_rolloff",
-    "mfccs",
     "chromagram"
 ]
 
-with open(os.path.join(os.path.dirname(__file__), "..", "environment.json"), "r") as f:
-    env = json.load(f)
+# MIDI Features
+MidiFeatureName = Literal[
+    "total_used_pitch",
+    "bar_used_pitch",
+    "total_used_note",
+    "bar_used_note",
+    "bar_pitch_class_histogram",
+    "pitch_range",
+    "avg_pitch_shift",
+    "avg_IOI",
+    "note_length_hist"
+]
 
-loggers_dict = {}
+
+class Environment(TypedDict):
+    APP_SERVER_PORT: int
+    DEBUG: bool
+    DATABASE_USER: str
+    DATABASE_PASSWORD: str
+    DATABASE_HOST: str
+    DATABASE_PORT: int
+    DATASET_TYPE: str
+    DATASET_PATH: str
+    client_id: Optional[str]
+    client_secret: Optional[str]
+    slack_notification_url: Optional[str]
+    slack_notification_user: Optional[str]
+
+
+with open(os.path.join(os.path.dirname(__file__), "..", "environment.json"), "r") as f:
+    env: Environment = json.load(f)
+
+loggers_dict: Dict[str, logging.Logger] = {}
 
 
 def create_logger(name: str) -> logging.Logger:
     global loggers_dict
-    if name in loggers_dict:
+    if name in loggers_dict.keys():
         return loggers_dict[name]
     else:
         logger = logging.getLogger(name)
@@ -59,6 +88,24 @@ def get_md5_from_path(file_path: str) -> str:
 
 def get_md5_from_midifile(m: MidiFile) -> str:
     return hashlib.md5(pickle.dumps(m)).hexdigest()
+
+
+def get_midi_path_from_md5(md5: str) -> str:
+    p = env["DATASET_PATH"].split(os.path.sep)
+    if env["DATASET_TYPE"] == "MMD":
+        p = os.path.sep + os.path.join(*p, "MMD_MIDI",
+                                       md5[0], md5[1], md5[2], md5 + ".mid")
+    else:
+        p = os.path.sep + \
+            os.path.join(*p, md5[0], md5[1], md5[2], md5 + ".mid")
+    return p
+
+
+def get_midi_path_from_md5_with_check(md5: str) -> Optional[str]:
+    p = get_midi_path_from_md5(md5)
+    if os.path.exists(p):
+        return p
+    return
 
 
 SLACK_URL = env.get("slack_notification_url", None)
