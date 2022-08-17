00a164b1298cda057f383b290f9db0c786d0cd2a
atsukoba <atsuya_wgp4346@keio.jp>
2022-08-18 05:48:49 +0900

    Sphere mapping

--- web_interface/components/PointsViewer.tsx
+++ web_interface/components/PointsViewer.tsx
@@ -1,30 +1,30 @@
-// @ts-nocheck
-
 import {
   Dispatch,
+  MutableRefObject,
   SetStateAction,
-  useState,
-  useRef,
-  ForwardRefExoticComponent,
   useEffect,
+  useRef,
+  useState,
 } from "react";
-import { MathUtils, Vector3 } from "three";
+import { AxesHelper, Color, Vector3 } from "three";
 
 import {
   GizmoHelper,
   GizmoViewport,
   OrbitControls,
-  OrbitControlsProps,
-  Point,
-  PointMaterial,
-  Points,
+  RoundedBox,
+  Stats,
+  Text,
+  Circle,
+  Sphere,
 } from "@react-three/drei";
 import { Canvas, useThree } from "@react-three/fiber";
 
 import { ResponseDatum } from "../api/data";
 import { calcMappingCoordinates } from "../utils/context";
 import SpotifyPlayer from "./SpotifyPlayer";
-import { useFrame } from "@react-three/fiber";
+
+const IS_DEVELOP_MODE = true;
 
 declare global {
   interface Window {
@@ -39,14 +39,15 @@ interface IProps {
   sidMapping: Map<string, string>;
 }
 
+const redColor = new Color(0xff0000);
+const blueColor = new Color(0x0000ff);
+
 export default function PointsViewer({ newData, sidMapping }: IProps) {
   const positions = calcMappingCoordinates(newData);
   console.log(positions);
   // states
+  const [isAutoRotating, setIsAutoRotating] = useState(false);
   const [currentTrackId, setCurrentTrackId] = useState("");
-  // const { range } = useControls({
-  //   range: { value: positions.length / 2, min: 0, max: positions.length },
-  // });
 
   return (
     <>
@@ -54,19 +55,29 @@ export default function PointsViewer({ newData, sidMapping }: IProps) {
         resize={{ scroll: true, debounce: { scroll: 10, resize: 0 } }}
         raycaster={{ params: { Points: { threshold: 0.175 } } }}
         dpr={[1, 2]}
-        camera={{ position: [0, 0, 70] }}
+        camera={{ position: [0, 0, 80] }}
         style={{
           width: "100%",
           height: "calc(100% - 100px)",
           backgroundColor: "#f7f7f7",
         }}
+        onPointerEnter={() => {
+          setIsAutoRotating(false);
+        }}
+        onPointerLeave={() => {
+          setIsAutoRotating(true);
+        }}
       >
+        {IS_DEVELOP_MODE && <Stats showPanel={0} className="stats" />}
         <OrbitControls
           enableDamping={false}
+          maxZoom={100}
+          minZoom={0.8}
           zoomSpeed={1}
-          autoRotate={true}
-          autoRotateSpeed={10}
+          autoRotate={isAutoRotating}
+          autoRotateSpeed={5}
         />
+        <primitive object={new AxesHelper(100)} />
         <GizmoHelper
           alignment="bottom-right" // widget alignment within scene
           margin={[80, 80]} // widget margins (X, Y)
@@ -89,6 +100,7 @@ export default function PointsViewer({ newData, sidMapping }: IProps) {
             />
           );
         })}
+        s
       </Canvas>
       <SpotifyPlayer track_id={currentTrackId} />
     </>
@@ -109,20 +121,65 @@ const CliclableBox = ({ x, y, z, data, setSidFunc }: IPropsClickableBox) => {
   // useFrame(() => {
   //   ref.current.rotation.x += 0.01;
   //   ref.current.rotation.y += 0.01;
-  // }, []);
+  // }, []);\
+
+  const useCircle = false;
+
   return (
-    <mesh
-      // ref={ref}
-      onPointerOver={() => setIsHovered(true)}
-      onPointerOut={() => setIsHovered(false)}
-      position={new Vector3(x, y, z)}
-      onClick={(e) => {
-        e.stopPropagation();
-        setSidFunc(data.sid);
-      }}
-    >
-      <boxBufferGeometry />
-      <meshLambertMaterial color={isHovered ? 0x44c2b5 : 0x9178e6} />
-    </mesh>
+    <>
+      {useCircle ? (
+        <Circle
+          args={[1, 1]}
+          onPointerOver={(e) => {
+            e.stopPropagation();
+            console.log(data);
+            setIsHovered(true);
+          }}
+          onPointerOut={(e) => {
+            e.stopPropagation();
+            setIsHovered(false);
+          }}
+          position={new Vector3(x, y, z)}
+          onClick={(e) => {
+            e.stopPropagation();
+            console.log(data);
+            setSidFunc(data.sid);
+          }}
+        />
+      ) : (
+        <mesh
+          onPointerOver={(e) => {
+            e.stopPropagation();
+            console.log(data);
+            setIsHovered(true);
+          }}
+          onPointerOut={(e) => {
+            e.stopPropagation();
+            setIsHovered(false);
+          }}
+          position={new Vector3(x, y, z)}
+          onClick={(e) => {
+            e.stopPropagation();
+            console.log(data);
+            setSidFunc(data.sid);
+          }}
+        >
+          <Sphere args={[0.25]}>
+            <meshBasicMaterial color={isHovered ? "red" : "orange"} />
+          </Sphere>
+          {/* <RoundedBox args={[0.5, 0.5, 0.5]} radius={0.05}></RoundedBox> */}
+        </mesh>
+      )}
+      {isHovered && (
+        <Text
+          color="black"
+          anchorX="center"
+          anchorY="middle"
+          position={new Vector3(x, y + 0.05, z)}
+        >
+          {data.title} / {data.artist}
+        </Text>
+      )}
+    </>
   );
 };
