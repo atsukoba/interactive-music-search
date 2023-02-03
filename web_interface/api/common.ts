fb8aee01bda3754230729a657574dca6fe982ed0
atsukoba <atsuya_wgp4346@keio.jp>
2023-02-03 10:29:31 +0900

    api host check

--- web_interface/api/common.ts
+++ web_interface/api/common.ts
@@ -1,4 +1,5 @@
 export const apiUrl = process.env.API_URL || "http://localhost:9823";
+console.log("API URL is set to " + apiUrl);
 export const headers = new Headers({
   Accept: "application/json",
   "Content-Type": "application/x-www-form-urlencoded; charset=utf-8",
