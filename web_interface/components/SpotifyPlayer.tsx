81a553b7e96860e5545d245e86158fe51b4ba2eb
atsukoba <atsuya@sfc.keio.ac.jp>
2023-02-14 00:06:15 +0900

    WIP: auto play

--- web_interface/components/SpotifyPlayer.tsx
+++ web_interface/components/SpotifyPlayer.tsx
@@ -6,12 +6,31 @@ interface IProps {
 }
 
 export default function SpotifyPlayer({ track_id }: IProps) {
+
+  useEffect(() => {
+    // TODO: automatically start playing spotidy preview when clicking data point on the space
+    const iframeDom = document.querySelector(".spotify-player");
+    iframeDom && iframeDom.addEventListener("load", () => {
+      // Accessing iframe internal elements is blocked by browser with CORS policy
+      // const button = document.querySelector(".spotify-player").contentWindow.document.getElementByTagName("button");
+      // button && button.click && button.click();
+
+      // Try clicking element got with absolute position
+      const buttonElm = document.elementFromPoint(
+        window.innerWidth - 15,
+        window.innerHeight - 15) as HTMLButtonElement;
+      console.log(buttonElm);
+      buttonElm?.click();
+    })
+  }, [])
+
   return (
     <iframe
       title="Spotify Web Player"
+      className="spotify-player"
       src={`https://open.spotify.com/embed/track/${track_id}`}
       width={"50%"}
-      height={"100%"}
+      height={"80px"}  // fixed height
       frameBorder={0}
       allow={"allow"}
       style={{
